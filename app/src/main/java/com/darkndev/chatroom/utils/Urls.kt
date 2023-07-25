b0866cd7aaafb97612dfde4dcbcfa3c9957c87b0
Dushyant Kumar Behera <49820671+DarkNDev@users.noreply.github.com>
2023-07-26 00:55:15 +0530

    gradle update

--- app/src/main/java/com/darkndev/chatroom/utils/Urls.kt
+++ app/src/main/java/com/darkndev/chatroom/utils/Urls.kt
@@ -1,9 +1,11 @@
 package com.darkndev.chatroom.utils
 
+import com.darkndev.chatroom.BuildConfig
+
 object Urls {
     private const val SOCKET = "ws://"
     private const val HTTPS = "http://"
-    private const val BASE_URL = "192.168.1.10:8080"
+    private const val BASE_URL = BuildConfig.BASE_URL
     const val MESSAGES = "$HTTPS$BASE_URL/messages"
     const val CHAT = "$SOCKET$BASE_URL/chat"
 }
